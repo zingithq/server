48b1b2fb6cf7fef4f7c4d312f481f6943703b450
AdityaKG-169 <aditya.kg169@gmail.com>
2022-03-21 07:13:39 +0530

    Feat: Add boilerplate for index file with middlewares and home route

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, { Application } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import envConfig from './config/config';
+import dbConfig from './config/dbConfig';
+
+const app: Application = express();
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.enable('trust proxy'); // To log IP Address of the requests
+app.use(
+	morgan(
+		':date[iso] :remote-addr :method :url :status :res[content-length] - :response-time ms'
+	)
+);
+
+dbConfig(envConfig.MONGO_URI);
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Hello World' }));
+
+const { PORT }: { PORT: number } = envConfig;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
